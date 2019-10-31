8ca380132ced65ff9e568772f979567f9b6d00e8
George Minnigerode <george.minnigerode.17@cnu.edu>
2019-10-30 23:01:52 -0400

    Initial commit

--- FileIO.cpp
+++ FileIO.cpp
@@ -13,12 +13,28 @@ using namespace std;
 
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	ifstream inputStream(filename, ios_base::in);
+	if(inputStream.is_open()){
+		string line;
+		while(!inputStream.eof()){
+			getline(inputStream,line);
+			contents += line + "\n";
+		}
+		return SUCCESS;
+	}
+	return COULD_NOT_OPEN_FILE_TO_READ;
 }
 
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	ofstream outputStream(filename, ios_base::out);
+		if(outputStream.is_open()){
+			for(int i = 0; i<myEntryVector.size(); i++){
+				outputStream << myEntryVector[i] + "\n";
+			}
+			return SUCCESS;
+		}
+	return COULD_NOT_OPEN_FILE_TO_WRITE;
 }
 
 
